22c8573d7808d4c37259825a283ebd16e7729a3d
Celia Macrae <celiamacrae@gmail.com>
2020-04-15 13:40:56 -0400

    added info to about page

--- client/Components/About.js
+++ client/Components/About.js
@@ -4,8 +4,9 @@ const About = () => {
     return (
       <div id='about'>
         <br />
+        <h1>Hello, World. I am Celia Macrae.</h1>
         <h1>
-          Hello, World. I am Celia Macrae. I am a full-stack web developer based in New York, NY.
+          I am a full-stack web developer based in New York, NY. I recently graduated from Duke University (2019) with a Bachelors Degree in Computer Science and completed the Grace Hopper Program at Fullstack Academy to sharpen my tecnical skills. I am currently working as a Software Engineering Teaching Fellow at Fullstack Academy and seeking fulltime employment as a Software Engineer.
         </h1>
       </div>
     )
