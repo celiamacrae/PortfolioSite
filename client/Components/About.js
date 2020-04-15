c1b873b1eebfd599db1117de288cfb27a71572cc
Celia Macrae <celiamacrae@gmail.com>
2020-04-15 13:52:39 -0400

    added image to about page

--- client/Components/About.js
+++ client/Components/About.js
@@ -4,10 +4,11 @@ const About = () => {
     return (
       <div id='about'>
         <br />
-        <h1>Hello, World. I am Celia Macrae.</h1>
-        <h1>
-          I am a full-stack web developer based in New York, NY. I recently graduated from Duke University (2019) with a Bachelors Degree in Computer Science and completed the Grace Hopper Program at Fullstack Academy to sharpen my tecnical skills. I am currently working as a Software Engineering Teaching Fellow at Fullstack Academy and seeking fulltime employment as a Software Engineer.
-        </h1>
+        <h1>Hello, World.</h1>
+        <img id='self' src='../../celia.jpeg' />
+        <h3>
+        My name is Celia Macrae. I am a full-stack web developer based in New York, NY. I recently graduated from Duke University (2019) with a Bachelors Degree in Computer Science and completed the Grace Hopper Program at Fullstack Academy to sharpen my tecnical skills. I am currently working as a Software Engineering Teaching Fellow at Fullstack Academy and seeking fulltime employment as a Software Engineer.
+        </h3>
       </div>
     )
 }
