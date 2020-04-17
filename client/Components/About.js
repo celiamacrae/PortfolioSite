f656b0bc775d0687b323e28c40916cb6016ba730
Celia Macrae <celiamacrae@gmail.com>
2020-04-16 21:07:41 -0400

    style change

--- client/Components/About.js
+++ client/Components/About.js
@@ -6,7 +6,8 @@ const About = () => {
         <br />
         <h1>Hello, World.</h1>
         {/* <img id='self' src='../../celia.jpeg' /> */}
-        <img id='self' src='../../celia2.jpeg' />
+        {/* <img id='self' src='../../celia2.jpeg' /> */}
+        <img id='self' src='../../headshot2.jpg' />
         <h3>
         My name is Celia Macrae. I am a full-stack web developer based in New York, NY. I recently graduated from Duke University (2019) with a Bachelors Degree in Computer Science and completed the Grace Hopper Program at Fullstack Academy to sharpen my tecnical skills. I am currently working as a Software Engineering Teaching Fellow at Fullstack Academy and seeking fulltime employment as a Software Engineer.
         </h3>
