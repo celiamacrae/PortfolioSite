78e42bd08870f96e53906b3abbde57ad7e68c7a9
Celia Macrae <celiamacrae@gmail.com>
2020-04-21 18:01:36 -0400

    changed project layout

--- client/Components/Projects.js
+++ client/Components/Projects.js
@@ -11,23 +11,28 @@ const Projects = () => {
           <h1 className='project_name'>GraphIt</h1>
           <h3 className='fullstackdev'>Fullstack Developer, 2020</h3>
           <br />
-          <p>GraphIt is a platform that transforms uploaded data from a non-technical user to “magically” produce the most appropriate graph in seconds.</p>
+          <p className='project_description'>GraphIt is a platform that transforms uploaded data from a non-technical user to “magically” produce the most appropriate graph in seconds.</p>
           <br />
+
 
           <div className='projinfo'>
 
-            <div>
+            <div className='pinfo'>
               <div className='repo'>
                 <img className='project_repo' src='https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png' />
                 <a href='https://github.com/celiamacrae/Graphit2.0'>Project Repo</a>
+                <img className='project_repo' src='https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png' />
               </div>
               <div className='deployedproj'>
                 <img className='project_img' src='https://avatars2.githubusercontent.com/u/60450249?s=400&u=b50a16566758429f3ee239735bdadd7ab61b9abc&v=4' />
                 <a href='https://graphit2.herokuapp.com/'>Check out GraphIt</a>
+                <img className='project_img' src='https://avatars2.githubusercontent.com/u/60450249?s=400&u=b50a16566758429f3ee239735bdadd7ab61b9abc&v=4' />
               </div>
             </div>
 
-            <div>
+            <br />
+
+            <div className='pinfo'>
               <p>GraphIt was built using:</p>
               <ul>
                 <li>PostgreSQL</li>
@@ -39,6 +44,7 @@ const Projects = () => {
               </ul>
             </div>
 
+
           </div>
 
           <br />
@@ -48,22 +54,26 @@ const Projects = () => {
           <h1 className='project_name'>Grace Shopper</h1>
           <h3 className='fullstackdev'>Fullstack Developer, 2020</h3>
           <br />
-          <p>Grace Shopper is eCommerce web application where users can purchase groceries and recipes.</p>
+          <p className='project_description'>Grace Shopper is eCommerce web application where users can purchase groceries and recipes.</p>
           <br />
 
           <div className='projinfo'>
-            <div>
+            <div className='pinfo'>
               <div className='repo'>
                 <img className='project_repo' src='https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png' />
                 <a href='https://github.com/1911-GH-mushrooms/GraceShopper'>Project Repo</a>
+                <img className='project_repo' src='https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png' />
               </div>
               <div className='deployedproj'>
                   <img className='project_img' src='https://avatars3.githubusercontent.com/u/59840616?s=200&v=4' />
                   <a href='https://graceshopper2020mushroom.herokuapp.com/home'>Check out Grace Shopper</a>
+                  <img className='project_img' src='https://avatars3.githubusercontent.com/u/59840616?s=200&v=4' />
               </div>
             </div>
 
-            <div>
+            <br />
+
+            <div className='pinfo'>
               <p>Grace Shopper was built using:</p>
               <ul>
                 <li>PostgreSQL</li>
@@ -77,7 +87,7 @@ const Projects = () => {
             </div>
 
 
-            <br />
+            {/* <br /> */}
             </div>
         </div>
 
@@ -85,21 +95,24 @@ const Projects = () => {
           <h1 className='project_name'>Study Buddy</h1>
           <h3 className='fullstackdev'>Fullstack Developer, 2020</h3>
           <br />
-          <p>Study Buddy is an IOS meetup app for study groups that allows you to create and attend events.</p>
+          <p className='project_description'>Study Buddy is an IOS meetup app for study groups that allows you to create and attend events.</p>
           <br />
 
           <div className='projinfo'>
-            <div>
+            <div className='pinfo'>
               <div className='repo'>
                 <img className='project_repo' src='https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png' />
                 <a href='https://github.com/Stackathon-Studdy-Buddy/Study-Buddy'>Project Repo</a>
+                <img className='project_repo' src='https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png' />
               </div>
               <div className='deployedproj'>
                 <img className='project_img' src='https://avatars1.githubusercontent.com/u/60267185?s=400&u=6bdcd2359d29593392d41f452213606b43cd3f92&v=4' />
               </div>
             </div>
 
-            <div>
+            <br />
+
+            <div className='pinfo'>
               <p>Study Buddy was built using:</p>
               <ul>
                 <li>Firebase Services</li>
